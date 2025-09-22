4938529878474763155d35ce08c77682ca169ae3
mahmoud <mahmoud.atia.ead@gmail.com>
2025-09-22 17:12:48 +0300

    templates

--- app/[locale]/(protected)/export/[id]/templates.js
+++ app/[locale]/(protected)/export/[id]/templates.js
@@ -95,6 +95,73 @@ export const MOCK_TEMPLATES_WITH_THEMES = [
     ],
     defaultFont: 'crimson-lato'
   },
-
+{
+    "id": "t8",
+    "name": "Soft Professional",
+    "value": "soft",
+    "previewUrl": "https://s3.resume.io/uploads/examples/resume/resume_pages/1123/persistent-resource/data-analyst-resume-example.jpg",
+    "themes": [
+      { "name": "Slate Blue", "value": "theme-slate-blue", "color": "#a9cce3" },
+      { "name": "Dusty Rose", "value": "theme-dusty-rose", "color": "#e6b0aa" },
+      { "name": "Sage Green", "value": "theme-sage-green", "color": "#a9dfbf" },
+      { "name": "Lavender", "value": "theme-lavender", "color": "#d7bde2" }
+    ],
+    "fonts": [
+      { "name": "Nunito + Raleway", "value": "nunito-raleway", "primary": "Raleway", "secondary": "Nunito", "description": "Soft and friendly" },
+      { "name": "Lato + Montserrat", "value": "lato-montserrat", "primary": "Montserrat", "secondary": "Lato", "description": "Modern and versatile" },
+      { "name": "Inter + Poppins", "value": "inter-poppins", "primary": "Poppins", "secondary": "Inter", "description": "Professional and clear" },
+      { "name": "Quicksand + Rubik", "value": "quicksand-rubik", "primary": "Rubik", "secondary": "Quicksand", "description": "Rounded and unique" }
+    ],
+    "defaultFont": "nunito-raleway"
+  },
+  {
+    id: 't5',
+    name: 'Monoline Compact',
+    value: 'template_minimal3',
+    previewUrl: '/templates/minimal3.jpg',
+    themes: [
+      { name: 'Slate Gray', value: 'theme-slate-gray', color: '#5d6d7e' },
+      { name: 'Burgundy', value: 'theme-burgundy', color: '#88304e' },
+      { name: 'Forest Green', value: 'theme-forest-green', color: '#2e8b57' },
+      { name: 'Deep Purple', value: 'theme-deep-purple', color: '#6a0dad' },
+    ],
+    fonts: [
+      { name: 'Fira Sans + Merriweather', value: 'fira-merriweather', primary: 'Fira Sans', secondary: 'Merriweather', description: 'Modern & classic' },
+      { name: 'Lato + Roboto Slab', value: 'lato-roboto-slab', primary: 'Lato', secondary: 'Roboto Slab', description: 'Clean & sturdy' },
+      { name: 'Inter + Lora', value: 'inter-lora', primary: 'Inter', secondary: 'Lora', description: 'Digital & elegant' },
+      { name: 'Open Sans + Playfair', value: 'open-sans-playfair', primary: 'Open Sans', secondary: 'Playfair Display', description: 'Friendly & stylish' },
+    ],
+    defaultFont: 'fira-merriweather'
+  },
+  {
+    id: 't6',
+    name: 'Gridline Modern',
+    value: 'template_minimal2',
+    previewUrl: '/templates/minimal2.jpg',
+    themes: [
+      { name: 'Slate & Crimson', value: 'theme-slate-crimson', color: '#DC2626' },
+      { name: 'Forest & Mint', value: 'theme-forest-mint', color: '#10B981' },
+      { name: 'Midnight & Gold', value: 'theme-midnight-gold', color: '#D97706' },
+    ],
+    fonts: [
+      { name: 'Roboto + Raleway', value: 'roboto-raleway', primary: 'Roboto', secondary: 'Raleway', description: 'Modern & geometric' },
+      { name: 'Lato + Merriweather', value: 'lato-merriweather', primary: 'Lato', secondary: 'Merriweather', description: 'Classic & readable' },
+      { name: 'Source Sans + Oswald', value: 'sourcesans-oswald', primary: 'Source Sans Pro', secondary: 'Oswald', description: 'Clean & bold' },
+    ],
+    defaultFont: 'roboto-raleway'
+  },
+  {
+    id: 't7',
+    name: 'Asymmetrical',
+    value: 'template_asymmetrical',
+    previewUrl: '/templates/asymmetrical.jpg',
+    themes: [
+      { name: 'Burnt Orange', value: 'theme-burnt-orange', color: '#E2703A' },
+    ],
+    fonts: [
+      { name: 'Open Sans + Montserrat', value: 'opensans-montserrat', primary: 'Open Sans', secondary: 'Montserrat', description: 'Modern & versatile' },
+    ],
+    defaultFont: 'opensans-montserrat'
+  }
 ];
 
